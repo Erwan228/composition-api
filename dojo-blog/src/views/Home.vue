5bbc41e18b07122fa0d333887aed405c4fed5217
Erwan Foxtail <84564131+Erwan228@users.noreply.github.com>
2023-06-27 14:08:07 +0200

    Update Home.vue

--- dojo-blog/src/views/Home.vue
+++ dojo-blog/src/views/Home.vue
@@ -1,29 +1,79 @@
 <template>
   <div class="home">
-    hoe
-    <p>My name is {{ name }} and my age is {{ age }}</p>
+    <!-- hoe
+    <p ref="p">My name is {{ name }} and my age is {{ age }}</p>
     <button @click="handleClick">click me</button>
+    <button @click="age++">add 1 to age</button>
+    <input type="text" v-model="name"> -->
+    <h1>Home</h1>
+    <!-- <h2>Refs</h2>
+    <p>{{ scholarOne.name }} - {{ scholarOne.age }}</p>
+    <button @click="updateScholarOne">Update scholar one</button>
+    <h2>Reactive</h2>
+    <p>{{ scholarTwo.name }} - {{ scholarTwo.age }}</p>
+    <button @click="updateScholarTwo">Update scholar two</button> -->
+    <input type="text" v-model="search">
+    <p>search term - {{ search }}</p>
+    <div v-for="name in names" :key="name">{{ name }}</div>
   </div>
 </template>
 
 <script>
+import { computed, ref } from 'vue';
+
+// import { ref, reactive } from 'vue'
+
 
 export default {
   name: 'Home',
   setup() {
-    console.log(this)
+    //console.log(this)
 
-    let name = 'Mario'
-    let age = 30
+    //const p = ref(null)
 
-    const handleClick = () => {
-      console.log('you clicked me')
-    }
+    // const name = ref('Mario')
+    // const age = ref(30)
+
+    // const handleClick = () => {
+    // console.log(p, p.value)
+    // p.value.classList.add('test')
+    // p.value.textContent = 'hello, scholars'
+    // name = 'Luigi'
+    //   name.value = 'Luigi'
+    //   age.value = 35
+    //}
+
+    // const scholarOne = ref({ name: 'Mario', age: 30 })
+    // const scholarTwo = reactive({ name: 'Luigi', age: 35 })
+
+    // const updateScholarOne = () => {
+    //   scholarOne.value.age = 40
+    // }
+
+    // const updateScholarTwo = () => {
+    //   scholarTwo.age = 45
+    // }
+
+    // const name = computed(() => {
+    //   return 'Erwan'
+    // })
+
+    const search = ref('')
+    const names = ref(['Mario', 'Yoshi P', 'Luigi', 'Toad', 'Bowser', 'Koopa', 'Peach'])
+
+
 
     return{
-       name, //kan også bruke name: name
-       age,
-       handleClick,
+      //  name, //kan også bruke name: name
+      //  age,
+      //  handleClick,
+      //p,
+      // scholarOne,
+      // updateScholarOne,
+      // scholarTwo,
+      // updateScholarTwo,
+      names,
+      search,
     }
   }
 }
